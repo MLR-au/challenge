d3f0b9fdde178c23cd5d7dc63706ff5c0d59aa1c
Marco La Rosa <m@lr.id.au>
2015-04-06 20:47:59 +1000

    add a configuration file

---
+++ app/scripts/services/configuration.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name challengeApp.configuration
+ * @description
+ * # configuration
+ * Constant in the challengeApp.
+ */
+angular.module('challengeApp')
+  .constant('configuration', {
+      //'layers': [ 'neighborhoods', 'streets', 'arteries', 'freeways' ],
+      'layers': [ 'neighborhoods', 'arteries', 'freeways' ]
+
+
+  });

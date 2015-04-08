b406f30cfca68e9866d0648785f354f149056eed
Marco La Rosa <m@lr.id.au>
2015-04-08 16:47:16 +1000

    add data directory to copy files task

--- Gruntfile.js
+++ Gruntfile.js
@@ -365,7 +365,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'data'
           ]
         }, {
           expand: true,

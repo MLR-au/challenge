a18ec5d3d45cd1e5f4c527737485a648a290a02b
Marco La Rosa <m@lr.id.au>
2015-04-08 15:53:19 +1000

    controller cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,19 +8,6 @@
  * Controller of the challengeApp
  */
 angular.module('challengeApp')
-  .controller('MainCtrl', [ '$scope', '$window', '$log', function ($scope, $window, $log) {
+  .controller('MainCtrl', [ '$scope', function ($scope) {
 
-      var width = $window.innerWidth,
-          height = $window.innerHeight;
-
-      // we'll pass the scale and transform from the neighborhood
-      //  render back via this object
-      $scope.translate = {};
-
-      // define the dimensions of the svg; the layers need this to compute
-      //  the center
-      $scope.dimensions = { 'width': width, 'height': height }
-
-      // the layers we want drawn over the top
-      $scope.layers = [ 'streets', 'arteries', 'freeways' ];
   }]);
